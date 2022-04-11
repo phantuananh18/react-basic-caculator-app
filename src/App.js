2a796d5ea65df6d1c0e62ba554ccc1646c0d522d
phantuananh18 <tuananh.61779@gmail.com>
2022-04-11 22:02:18 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [result, setResult] = useState("");
+
+  const onChangeResult = (event) => {
+    setResult(event.target.name);
+  }
+
+  const onClearResult = () => {
+    setResult("");
+  }
+
+  const onBackSpace = () => {
+    setResult(result.slice(0, result.length - 1));
+  }
+
+  const onHandleClick = (e) => {
+    setResult(result.concat(e.target.name));
+  }
+
+  const onCaculate = () => {
+    try {
+      setResult(eval(result).toString());
+    } catch (err) {
+      setResult('Error');
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="basic-caculator">
+      <form>
+        <input type='text' value={result} onChange={onChangeResult} />
+      </form>
+
+      <div className='keyboard'>
+        <button className='highlight' id='clear' onClick={onClearResult}>Clear</button>
+        <button className='highlight' id='backspace' onClick={onBackSpace} name='C'>C</button>
+        <button className='highlight' onClick={onHandleClick} name='/'>&divide;</button>
+        <button onClick={onHandleClick} name='7'>7</button>
+        <button onClick={onHandleClick} name='8'>8</button>
+        <button onClick={onHandleClick} name='9'>9</button>
+        <button className='highlight' onClick={onHandleClick} name='*'>&times;</button>
+        <button onClick={onHandleClick} name='4'>4</button>
+        <button onClick={onHandleClick} name='5'>5</button>
+        <button onClick={onHandleClick} name='6'>6</button>
+        <button className='highlight' onClick={onHandleClick} name='-'>&ndash;</button>
+        <button onClick={onHandleClick} name='1'>1</button>
+        <button onClick={onHandleClick} name='2'>2</button>
+        <button onClick={onHandleClick} name='3'>3</button>
+        <button className='highlight' onClick={onHandleClick} name='+'>+</button>
+        <button onClick={onHandleClick} name='0'>0</button>
+        <button onClick={onHandleClick} name='.'>.</button>
+        <button className='highlight' id='result' onClick={onCaculate} name='='>=</button>
+      </div>
+
     </div>
   );
 }
